f16e6c3d0f34bca38b59bf0e3d38803ee79471c6
roney ruiz <roneyruizrojas@gmail.com>
2026-08-09 11:27:26 -0600

    chore(engine): discard old queue implementation

--- projects/poc-engine/source/core/queues/queue.errors.ts
+++
@@ -1,6 +0,0 @@
-export class QueueEnqueueError extends Error {
-  constructor(message: string, options?: { cause?: unknown }) {
-    super(message, options);
-    this.name = "QueueEnqueueError";
-  }
-}
