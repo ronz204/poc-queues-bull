18fb5365c60578ce9e1f3d7ee02da1ece753befa
roney ruiz <roneyruizrojas@gmail.com>
2026-09-22 16:06:00 -0600

    infra(deps): install drizzle components and config

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+	strict: true,
+	verbose: true,
+	dialect: "postgresql",
+	out: "./drizzle/migrations/",
+	schema: "./drizzle/database/**/*.model.ts",
+	migrations: { schema: "drizzle", table: "migrations" },
+	dbCredentials: { url: process.env.POSTGRES_SAMPLER_URL! },
+});
